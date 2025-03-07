62505325f044d412dba44badee6d45e7d6c9e386
utsx <dimma1506@icloud.com>
2025-03-07 23:51:58 +0300

    Jstyle

--- server/src/main/java/ru/utsx/controller/MetricController.java
+++ server/src/main/java/ru/utsx/controller/MetricController.java
@@ -6,7 +6,6 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 import ru.utsx.model.CarMetric;
 
 @Controller
